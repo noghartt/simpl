1a817d2775276bac0c1d1edbc29ab3cdb0f132c6
Guilherme Ananias <hi@noghartt.dev>
2023-05-07 02:27:40 -0300

    typecheck stuffs

--- lib/main.ml
+++ lib/main.ml
@@ -1,18 +1,18 @@
 open Ast
 open Interp
+open Typecheck
+
+let typecheck (e : expr) : expr =
+  ignore (typeof Env.empty e); e
 
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   ast
 
-let rec eval_small (e : expr) : expr =
-  if is_value e then e
-  else e |> step |> eval_small
-
 let eval (e : expr) : expr =
   if is_value e then e
-  else e |> step |> eval_big
+  else e |> step |> typecheck |> eval_big
 
 let interp (s : string) : expr =
   let ast = parse s in
